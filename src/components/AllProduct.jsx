3bb6ce95939a9e82a2b07ee9b7557858ac6ae703
akashsarker012 <akashsarker.dev@gmail.com>
2024-02-19 16:48:39 +0600

    Your commit message

--- src/components/AllProduct.jsx
+++ src/components/AllProduct.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Space, Table } from "antd";
+import axios from "axios";
 const columns = [
   {
     title: "Product Name",
@@ -8,14 +9,6 @@ const columns = [
       {
         text: "Joe",
         value: "Joe",
-      },
-      {
-        text: "Category 1",
-        value: "Category 1",
-      },
-      {
-        text: "Category 2",
-        value: "Category 2",
       },
     ],
     filterMode: "tree",
@@ -27,24 +20,19 @@ const columns = [
     title: "Image",
     dataIndex: "image",
     render: (_, record) => (
-        <img src="https://cdn.shopify.com/s/files/1/0471/6039/3896/products/MacBooK-Air-M1-Space-Gray-iStockBD.png?v=1649504754&width=600" alt="image" />
+        <img width={100} src="https://cdn.shopify.com/s/files/1/0471/6039/3896/products/MacBooK-Air-M1-Space-Gray-iStockBD.png?v=1649504754&width=600" alt="image" />
       ),
   },
   {
     title: "Store Name",
-    dataIndex: "address",
+    dataIndex: "storename",
+    render: (_, record) => ( <p>{record.store.storeName}</p>
+    //  console.log(record.store.storeName)
+    ),
     filters: [
       {
         text: "London",
         value: "London",
-      },
-      {
-        text: "New York",
-        value: "New York",
-      },
-      {
-        text: "Sydney",
-        value: "Sydney",
       },
     ],
     onFilter: (value, record) => record.address.startsWith(value),
@@ -64,36 +52,41 @@ const columns = [
     ),
   },
 ];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-  },
-  {
-    key: "4",
-    name: "Jim Red",
-    age: 32,
-    address: "London No. 2 Lake Park",
-  },
-];
+// const data = [
+//   {
+//     key: "1",
+//     name: "John Brown",
+//     age: 32,
+//     address: "New York No. 1 Lake Park",
+//   },
+
+// ];
 const onChange = (pagination, filters, sorter, extra) => {
   console.log("params", pagination, filters, sorter, extra);
 };
-const AllProduct = () => (
-  <Table columns={columns} dataSource={data} onChange={onChange} />
-);
-export default AllProduct;
+
+export default function AllProduct() {
+
+  const [productInfo,setProductInfo] = useState()
+
+  console.log(productInfo);
+
+  useEffect(()=>{
+async function Product() {
+ let productData = await axios.get("http://localhost:3000/api/v1/product/getallproduct")
+
+ setProductInfo(productData.data);
+}
+ Product()
+ 
+  },[])
+  return (
+    <div>
+
+      <Table columns={columns} dataSource={productInfo} onChange={onChange} />
+
+    </div>
+   
+  )
+}
+
